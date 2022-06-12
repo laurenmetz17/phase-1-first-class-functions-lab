38ad959ee12bdd9bf8c26b3f7e17972ee53688e0
Lauren Metz <Laurenmetz15@gmail.com>
2022-06-12 01:57:37 -0400

    commit tree 6149361553e07e83ebcced9813a2852bc9046cfa

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solution in this file!
+function returnFirstTwoDrivers(drivers) {
+    return drivers.slice(0,2);
+}
+
+function returnLastTwoDrivers(drivers) {
+    return drivers.slice(drivers.length - 2, drivers.length);
+
+}
+
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(num) {
+    return (function (fare) { 
+        return fare * num;
+    });
+}
+
+const fareDoubler = function(fare) {
+    let doubler = createFareMultiplier(2);
+    return doubler(fare);
+}
+
+const fareTripler = function(fare) {
+    let tripler = createFareMultiplier(3);
+    return tripler(fare);
+}
+
+function selectDifferentDrivers(drivers,func) {
+    return func(drivers);
+}
